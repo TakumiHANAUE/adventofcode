1787fe827d9f360e634c0110fa15cd213b0e7cb3
TakumiHANAUE <40872702+TakumiHANAUE@users.noreply.github.com>
2023-12-07 08:21:55 +0000

    update day1/day1_part2.nim

--- day1/day1_part2.nim
+++ day1/day1_part2.nim
@@ -4,7 +4,7 @@ import math
 type
     DigitString = tuple[word: string, valueStr: string]
 
-let digitWords: seq[DigitString] = @[
+const DIGIT_WORD_ARRAY: array[18, DigitString] = [
     ("one"  , "1"),
     ("two"  , "2"),
     ("three", "3"),
@@ -25,35 +25,51 @@ let digitWords: seq[DigitString] = @[
     ("9"    , "9")
 ]
 
-let inputFile = open("./day1_input.txt", fmRead)
-# let inputFile = open("./tmp.txt", fmRead)
+type
+    DigitPosition = tuple[index: int, valueStr: string]
 
-var calibvalues: seq[int] = @[]
-for line in inputfile.lines:
+proc getTwoDigitCandidate(line: string): seq[DigitPosition] = 
     # find "two-digit number" candidate
-    var digitsSeq: seq[tuple[index: int, valueStr: string]] = @[]
-    for d in digitWords:
+    var digitsCandidates: seq[tuple[index: int, valueStr: string]] = @[]
+    for d in DIGIT_WORD_ARRAY:
         if line.contains(d.word):
             # "first digit" candidate
-            digitsSeq.add( (line.find(d.word), d.valueStr) )
+            digitsCandidates.add( (line.find(d.word), d.valueStr) )
             # "last digit" candidate
-            digitsSeq.add( (line.rfind(d.word), d.valueStr) )
-    
-    # find first/last digit
+            digitsCandidates.add( (line.rfind(d.word), d.valueStr) )
+    return digitsCandidates
+
+proc getFirstDigit(line: string): string = 
+    let digits = line.getTwoDigitCandidate()
+    # find first digit
     var firstDigit = (index: line.high(), valueStr: "")
+    for d in digits:
+        if d.index <= firstDigit.index:
+            firstDigit.index = d.index
+            firstDigit.valueStr = d.valueStr
+    return firstDigit.valueStr
+
+proc getLastDigit(line: string): string = 
+    let digits = line.getTwoDigitCandidate()
+    # find lst digit
     var lastDigit = (index: 0, valueStr: "")
-    for dig in digitsSeq:
-        # find first digit
-        if dig.index <= firstDigit.index:
-            firstDigit.index = dig.index
-            firstDigit.valueStr = dig.valueStr
-        # find last digit
-        if dig.index >= lastDigit.index:
-            lastDigit.index = dig.index
-            lastDigit.valueStr = dig.valueStr
-    
+    for d in digits:
+        if d.index >= lastDigit.index:
+            lastDigit.index = d.index
+            lastDigit.valueStr = d.valueStr
+    return lastDigit.valueStr
+
+############
+
+let inputFile = open("./day1_input.txt", fmRead)
+
+var calibvalues: seq[int] = @[]
+for line in inputFile.lines:
+    # get first/last digit
+    let firstDigit = line.getFirstDigit()
+    let lastDigit = line.getLastDigit()
     # get calibration value
-    calibvalues.add(parseInt(firstDigit.valueStr & lastDigit.valueStr))
+    calibvalues.add(parseInt(firstDigit & lastDigit))
 
 close(inputFile)
 
